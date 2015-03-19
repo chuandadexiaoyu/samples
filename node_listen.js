509bd8bd5ea50024c8d95c564aa0b240bc1e2631
root <root@localhost.localdomain>
2015-03-19 17:55:23 +0800

    提交一些代码

--- node_listen.js
+++ node_listen.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  console.log("Start Connect!");
+var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Guest\n');
+  console.log("end request");
 }).listen(1337, "127.0.0.1");
